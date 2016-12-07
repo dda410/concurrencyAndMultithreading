6b301fa09769c916874e0e3dc40ed992ca88736a
dda410 <dimitri1215@gmail.com>
2016-12-07 18:56:44 +0100

    removing method working but is really bad, needs revising

--- src/data_structures/implementation/FineGrainedTree.java
+++ src/data_structures/implementation/FineGrainedTree.java
@@ -44,13 +44,12 @@ public class FineGrainedTree<T extends Comparable<T>> implements Sorted<T> {
   }
 
   public void add(T data) {
-    System.out.println(data);
     TreeNode newNode = new TreeNode(data);
     TreeNode curNode = null;
     TreeNode parentNode = null;
     int compare = 0;		
     // lock.lock();
-    if (root == null) {
+    if (root == null) {  // maybe call this empty method.
       //The tree is empty, insert the new node as the root
       root = newNode;
       // root.lock.unlock();
@@ -90,29 +89,158 @@ public class FineGrainedTree<T extends Comparable<T>> implements Sorted<T> {
     }
   }
 
-  public void add2(T e) {
-    root = add(root, e, root);
-  }
 
-  // Adding a node recursively
-  private TreeNode add(TreeNode root, T e, TreeNode parent) {
-    if (root == null) {
-      return new TreeNode(e);
-    }
-    if (e.compareTo(root.data) < 0) {
-      root.lock.lock();
-      root.left = add(root.left, e, root);
-      root.lock.unlock();
+
+
+  private TreeNode findReplacement(TreeNode subRoot) {		
+    TreeNode curNode = null;
+    TreeNode parentNode = null;
+    
+    if(subRoot.left != null) {
+      //Find the "biggest" node in the left subtree as the replacement
+      parentNode = subRoot;
+      curNode = subRoot.left;
+      curNode.lock.lock();
+      while(curNode.right != null) {
+        if(parentNode != subRoot) {
+          parentNode.lock.unlock();
+        }
+        parentNode = curNode;
+        curNode = curNode.right;
+        curNode.lock.lock();
+      }
+      if (curNode.left != null) {
+        curNode.left.lock.lock();
+      }
+      if (parentNode == subRoot) {
+        parentNode.left = curNode.left;
+      } else {
+        parentNode.right = curNode.left;
+        parentNode.lock.unlock();
+      }
+      if (curNode.left != null) {
+        curNode.left.lock.unlock();
+      }
+      curNode.lock.unlock();
+    } else if (subRoot.right != null) {
+      //Find the "smallest" node in the right subtree as the replacement
+      parentNode = subRoot;
+      curNode = subRoot.right;
+      curNode.lock.lock();
+      while (curNode.left != null) {
+        if(parentNode != subRoot) {
+          parentNode.lock.unlock();
+        }
+        parentNode = curNode;
+        curNode = curNode.left;
+        curNode.lock.lock();
+      }
+      if(curNode.right != null) {
+        curNode.right.lock.lock();
+      }
+      if(parentNode == subRoot)
+        parentNode.right = curNode.right;
+      else {
+        parentNode.left = curNode.right;
+        parentNode.lock.unlock();
+      }
+      if(curNode.right != null) {
+        curNode.right.lock.unlock();
+      }
+      curNode.lock.unlock();
     } else {
-      root.lock.lock();
-      root.right = add(root.right, e, root);
-      root.lock.unlock();
+      //No children, no replacement needed
+      return null;
     }
-    return root;
+    return curNode;
   }
 
-  public void remove(T t) {
-    throw new UnsupportedOperationException();
+  public void remove(T data) {		
+    TreeNode curNode = null;
+    TreeNode parentNode = null;
+    int compare = 0;
+    int oldCompare = 0;		
+    // headLock.lock();
+    if(root != null) {
+      //Tree is not empty, search for the passed data.  Start by checking
+      //the root separately.
+      curNode = root;
+      parentNode = curNode;
+      curNode.lock.lock();
+      compare = curNode.data.compareTo(data);
+      if(compare > 0) {
+        //root is "bigger" than passed data, search the left subtree
+        curNode = curNode.left;
+        oldCompare = compare;
+      } else if(compare < 0) {
+        //root is "smaller" than passed data, search the right subtree
+        curNode = curNode.right;
+        oldCompare = compare;
+      } else {
+        //Found the specified data, remove it from the tree
+        TreeNode replacement = findReplacement(curNode);				
+        root = replacement;
+        if(replacement != null) {
+          replacement.left = curNode.left;
+          replacement.right = curNode.right;
+        }				
+        curNode.lock.unlock();
+        // headLock.unlock();
+        return;
+      }
+      curNode.lock.lock();
+      // headLock.unlock();
+			
+      while(true) {
+        compare = curNode.data.compareTo(data);
+        if(compare != 0) {
+          parentNode.lock.unlock();
+          parentNode = curNode;
+          if(compare > 0) {
+            //curNode is "bigger" than passed data, search the left
+            //subtree
+            curNode = curNode.left;
+            oldCompare = compare;
+          } else if(compare < 0) {
+            //curNode is "smaller" than passed data, search the right
+            //subtree
+            curNode = curNode.right;
+            oldCompare = compare;
+          }
+        } else {
+          //Found the specified data, remove it from the tree
+          TreeNode replacement = findReplacement(curNode);
+					
+          //Set the parent pointer to the new child
+          if (oldCompare > 0) {
+            parentNode.left = replacement;
+          } else {
+            parentNode.right = replacement;
+          }					
+          //Replace curNode with replacement
+          if (replacement != null) {
+            replacement.left = curNode.left;
+            replacement.right = curNode.right;
+          }					
+          curNode.lock.unlock();
+          parentNode.lock.unlock();
+          return;
+        }
+				
+        if (curNode == null) {
+          break;
+        } else {
+          curNode.lock.lock();
+        }
+      } // closing the while
+    } else {
+      // Tree is empty
+      // headLock.unlock();
+      return;
+    }
+		
+    //The specified data was not in the tree
+    parentNode.lock.unlock();
   }
 
   // Visiting the tree nodes recursively with inorder traversal 
